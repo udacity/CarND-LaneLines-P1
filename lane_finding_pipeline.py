4e277b2985b4d9d01065c34f0fe171c0589036e6
Matthew Chan <tuzzer@gmail.com>
2017-03-20 00:59:39 -0700

    adjusted the parameters. deleted comments populated ipython notebook.

--- lane_finding_pipeline.py
+++ lane_finding_pipeline.py
@@ -10,33 +10,18 @@ from moviepy.editor import VideoFileClip
 
 
 def grayscale(img):
-    """Applies the Grayscale transform
-    This will return an image with only one color channel
-    but NOTE: to see the returned image as grayscale
-    (assuming your grayscaled image is called 'gray')
-    you should call plt.imshow(gray, cmap='gray')"""
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # Or use BGR2GRAY if you read an image with cv2.imread()
-    # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
 def canny(img, low_threshold, high_threshold):
-    """Applies the Canny transform"""
     return cv2.Canny(img, low_threshold, high_threshold)
 
 
 def gaussian_blur(img, kernel_size):
-    """Applies a Gaussian Noise kernel"""
     return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
 
 
 def region_of_interest(img, vertices):
-    """
-    Applies an image mask.
-
-    Only keeps the region of the image defined by the polygon
-    formed from `vertices`. The rest of the image is set to black.
-    """
     # defining a blank mask to start with
     mask = np.zeros_like(img)
 
@@ -60,23 +45,10 @@ def get_line_eqn(x1, y1, x2, y2):
     b = y1 - m*x1
     return m, b
 
-def draw_lines(img, lines, vertical_limits, color=(255, 0, 0), thickness=2):
-    """
-    NOTE: this is the function you might want to use as a starting point once you want to
-    average/extrapolate the line segments you detect to map out the full
-    extent of the lane (going from the result shown in raw-lines-example.mp4
-    to that shown in P1_example.mp4).
+def draw_lines(img, lines, vertical_limits, color=(255, 0, 0), thickness=2, min_slope=0):
 
-    Think about things like separating line segments by their
-    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
-    line vs. the right line.  Then, you can average the position of each of
-    the lines and extrapolate to the top and bottom of the lane.
-
-    This function draws `lines` with `color` and `thickness`.
-    Lines are drawn on the image inplace (mutates the image).
-    If you want to make the lines semi-transparent, think about combining
-    this function with the weighted_img() function below
-    """
+    if lines is None or not isinstance(vertical_limits, (tuple, list)):
+        return
 
     # Group similar lines
     left_lane_lines = []
@@ -84,9 +56,10 @@ def draw_lines(img, lines, vertical_limits, color=(255, 0, 0), thickness=2):
 
     for line in lines:
         line = line[0]
-        if get_line_eqn(*line)[0] > 0:
+        slope = get_line_eqn(*line)[0]
+        if slope > min_slope:
             left_lane_lines.append(line)
-        else:
+        elif slope < -min_slope:
             right_lane_lines.append(line)
 
     # Average similar lines
@@ -103,8 +76,16 @@ def draw_lines(img, lines, vertical_limits, color=(255, 0, 0), thickness=2):
     y1, y2 = vertical_limits
     for lane_line in lane_lines:
         m, b = get_line_eqn(*lane_line)
-        x1 = (y1 - b) / m
-        x2 = (y2 - b) / m
+
+        # This is to prevent divide-by-zero exception
+        if m == 0:
+            m = 0.0000001
+
+        if m == float("Inf") or m == -float("Inf"):
+            x1 = x2 = lane_line[0]
+        else:
+            x1 = (y1 - b) / m
+            x2 = (y2 - b) / m
         extrapolated_lines.append(np.array([x1, y1, x2, y2], dtype=int))
 
     for line in extrapolated_lines:
@@ -113,30 +94,14 @@ def draw_lines(img, lines, vertical_limits, color=(255, 0, 0), thickness=2):
 
 
 def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap, vertical_limits):
-    """
-    `img` should be the output of a Canny transform.
-
-    Returns an image with hough lines drawn.
-    """
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len,
                             maxLineGap=max_line_gap)
     line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
-    draw_lines(line_img, lines, vertical_limits)
+    draw_lines(line_img, lines, vertical_limits, min_slope=img.shape[0]/img.shape[1])
     return line_img
 
 
 def weighted_img(img, initial_img, alpha=0.8, beta=1., gamma=0.):
-    """
-    `img` is the output of the hough_lines(), An image with lines drawn on it.
-    Should be a blank image (all black) with lines drawn on it.
-
-    `initial_img` should be the image before any processing.
-
-    The result image is computed as follows:
-
-    initial_img * α + img * β + λ
-    NOTE: initial_img and img must be the same shape!
-    """
     return cv2.addWeighted(initial_img, alpha, img, beta, gamma)
 
 
@@ -149,23 +114,23 @@ def lane_finding(raw_image):
     image = gaussian_blur(image, 5)
 
     # Apply Canny filter
-    image = canny(image, 50, 150)
+    image = canny(image, 30, 150)
 
     # Crop up the region of interest
     image_height, image_width = image.shape
     top_left = (0.46*image_width, 0.60*image_height)
     top_right = (0.51*image_width, 0.60*image_height)
-    bottom_left = (0*image_width, image_height)
-    bottom_right = (image_width, image_height)
+    bottom_left = (0.2*image_width, 0.9*image_height)
+    bottom_right = (0.88*image_width, 0.9*image_height)
     vertices = np.array([[bottom_left, top_left, top_right, bottom_right]], dtype=np.int32)
     image = region_of_interest(image, vertices)
 
     # Apply Hough Transform to get lanes
     rho = 2  # distance resolution in pixels of the Hough grid
     theta = np.pi * 1 / 180  # angular resolution in radians of the Hough grid
-    threshold = 50  # minimum number of votes (intersections in Hough grid cell)
-    min_line_length = 60  # minimum number of pixels making up a line
-    max_line_gap = 50  # maximum gap in pixels between connectable line segments
+    threshold = 30  # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 30  # minimum number of pixels making up a line
+    max_line_gap = 60  # maximum gap in pixels between connectable line segments
     image = hough_lines(image, rho, theta, threshold, min_line_length, max_line_gap,
                         vertical_limits=(bottom_left[1], top_left[1]))
 
