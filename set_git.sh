33ab063ccc8decc3eca7bc9a5d13300d05e35b17
citlaligm <gmtlali88@gmail.com>
2018-06-12 23:56:02 +0000

    Add git pull and fix echo of username and email

--- set_git.sh
+++ set_git.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# Make sure you have the latest version of the repo
+echo
+git pull
+echo
+
 # Ask the user for login details
 read -p 'Git repository url: ' upstreamVar
 read -p 'Git Username: ' userVar
 read -p 'Git email: ' emailVar
 
 echo
-echo Thankyou $userVar, we now have your credentials
+echo Thank you $userVar!, we now have your credentials
 echo for upstream $upstreamVar. You must supply your password for each push.
 echo
 
@@ -21,6 +26,6 @@ echo Please verify remote:
 git remote -v
 echo
 
-echo Please verify credentials:
-echo username: git config user.name
-echo email git config user.email
+echo Please verify your credentials:
+echo username: `git config user.name`
+echo email: `git config user.email`
